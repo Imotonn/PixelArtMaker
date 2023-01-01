94c4c8b735c4108567136815d8fa771c9d3da1a7
Imotonn <87901880+Imotonn@users.noreply.github.com>
2023-01-02 05:35:52 +1100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,5 +1,5 @@
 // Select color input
-const color = document.getElementById('colorPicker').value;
+const color = document.getElementById('colorPicker');
 
 // Select size input
 const sizeForm = document.getElementById('sizePicker');
@@ -8,32 +8,35 @@ const width = document.getElementById('inputWidth');
 
 // Format grid
 const grid = document.getElementById('pixelCanvas');
-grid.style.borderWidth = "5px"; //DEBUG
+grid.style.borderWidth = '3px'; //DEBUG
 
-
-// When size is submitted by the user, call makeGrid()
+// Function for drawing an empty grid
 function makeGrid(row,column) {
-    grid.style.display = "none";
+    grid.style.display = 'none';
+    var gridWidth = 20 * column; 
+    grid.width = gridWidth; 
     for (var i=0; i<row; i++) {
         grid.insertRow(i);
         for (var j=0; j<column; j++) {
             grid.rows[i].insertCell(j);
         }
     }
-    grid.style.display = "block";
+    grid.style.display = 'block';
 }
 
-sizeForm.addEventListener("submit", function(event) {
+// When size is submitted by the user, call makeGrid()
+sizeForm.addEventListener('submit', function(event) {
     event.preventDefault();
-    grid.innerHTML = "";
+    grid.innerHTML = '';
     const row = height.value;
     const column = width.value;
     makeGrid(row,column);
 });
 
-grid.addEventListener("click", function(event) {
-    console.log("inside event listener") //DEBUG
-    // event.target.style.backgroundColor = "red";  //DEBUG -> works
-    event.target.style.backgroundColor = color;     
-
+// Change cell background color when clicked
+grid.addEventListener('click', function(event) {
+    if (event.target.nodeName.toLowerCase() === 'td' ) {
+        event.target.style.backgroundColor = color.value; 
+    }
+        
 });
